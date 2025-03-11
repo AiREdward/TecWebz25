edc1daf417541725921004bcddc4d1bb40eb18a8
AiREdward <stefano.baso@studenti.unipd.it>
2025-03-11 11:35:09 +0100

    merge popup + stitemato stile + aggiunta icona + refactor file pt2

--- views/HomeView.php
+++ views/HomeView.php
@@ -7,6 +7,13 @@ class HomeView {
 <head>
     <meta charset="UTF-8">
     <title><?php echo $data['title']; ?></title>
+
+    <meta name="author" content="TODO">
+    <meta name="description" content="TODO">
+    <meta name="keywords" content="TODO">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
