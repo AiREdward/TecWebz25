0acca3c36797d8ea8cf757a98fa8a1aea9d6b953
AiREdward <stefano.baso@studenti.unipd.it>
2025-03-25 22:40:54 +0100

    eliminato voce menu non usata

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -5,7 +5,6 @@ $menu_items = [
     'Home' => 'index.php?page=home',
     'Shop' => 'index.php?page=shop',
     'Permuta' => 'index.php?page=trade',
-    'Torneo' => 'index.php?page=tournament',
     'Chi Siamo' => 'index.php?page=chi-siamo',
     'Contattaci' => 'index.php?page=contact'
 ];
