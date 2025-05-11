2128715561e64d2ab6f1e5a426694dbb62cf9e67
Alessandro <51910404+aledipa@users.noreply.github.com>
2025-05-11 16:11:10 +0200

    Sistemato bottoni accedi/logout (Desktop) su navbar e colore link visitati

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -24,15 +24,30 @@ $loggedIn = isset($_SESSION['user']); // Controlla se l'utente è loggato
         <?php endforeach; ?>
 
         <?php if ($ruolo === 'admin'): ?>
-            <li><a href="index.php?page=admin" target="_blank" class="<?= strpos($_SERVER['REQUEST_URI'], 'index.php?page=admin') !== false ? 'active' : '' ?> menu-item">Admin Dashboard</a></li>
+            <li><a href="index.php?page=admin" target="_blank" class="<?= strpos($_SERVER['REQUEST_URI'], 'index.php?page=admin') !== false ? 'active' : '' ?> menu-item">Cruscotto</a></li>
         <?php endif; ?>
+        
     </ul>
-    <div class="nomargin">
-        <?php if ($loggedIn): ?>
-            <a href="index.php?page=auth&action=logout">Logout</a>
-        <?php else: ?>
-            <a href="index.php?page=auth"><img src="assets/images/account.webp" class="icon-account-button" alt="Profilo"/></a>
-        <?php endif; ?>
+    <div class="nomargin log-section">
+        <ul>
+            <?php if ($loggedIn): ?>
+                <li>
+                    <a href="index.php?page=auth&action=logout" class="menu-button">
+                        <img src="assets/images/logout.webp" class="icon-button" alt="Logout"/>
+                         <!-- <i class="fas fa-logout" class="icon-button" aria-hidden="true"></i> -->
+                        <span class="logout-text">Logout</span>
+                    </a>
+                </li>
+            <?php else: ?>
+                <li>
+                    <a href="index.php?page=auth" class="menu-button">
+                        <img src="assets/images/account.webp" class="icon-button" alt="Profilo"/>
+                        <!-- <i class="fas fa-user" class="icon-button" aria-hidden="true"></i> -->
+                        <span class="logout-text">Accedi</span>
+                    </a>
+                </li>
+            <?php endif; ?>
+        </ul>
     </div>
 </nav>
 
