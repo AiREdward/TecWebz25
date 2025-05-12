a1a4b6af1f587e0ebe57701b15ae74e8a166dd92
Pietro Crotti <pietro.crotti03@gmail.com>
2025-05-12 15:33:33 +0200

    aggiornamento coordinate

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const lat = 45.661858;
-    const lng = 12.252263;
-    const map = L.map('map-container').setView([lat, lng], 17);
+    const lat = 45.41123820134286;
+    const lng = 11.887567492273224;
+    const map = L.map('map-container').setView([lat, lng], 19);
     
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -11,6 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
     const marker = L.marker([lat, lng]).addTo(map);
     marker.bindPopup("GameStart").openPopup();
 });
-
-
 
