bf7c9c2bd5eca4d2e1a9422150ee72d7d89e43bf
Pietro Crotti <pietro.crotti03@gmail.com>
2025-06-27 17:49:50 +0200

    fix accessibilita contattaci

--- models/ContactModel.php
+++ models/ContactModel.php
@@ -4,7 +4,7 @@ class ContactModel {
         return [
             'title' => 'Contattaci',
             'header' => 'Contattaci',
-            'phone' => '+39 349 1234567',
+            'phone' => '+393491234567',
             'email' => 'info@tecwebz25.it',
             'address' => 'Via Trieste, 63, 35131 Padova PD',
             'content' => 'Siamo entusiasti di offrirti un supporto completo e di accoglierti con il sorriso! Per qualsiasi informazione, suggerimento o richiesta di assistenza, non esitare a contattarci. Ci impegniamo a risponderti nel più breve tempo possibile e a fornirti tutta l\'assistenza di cui hai bisogno. Grazie per aver scelto di metterti in contatto con noi, siamo qui per aiutarti e rendere la tua esperienza il più piacevole possibile!',
